731a44d170f3150938bca801755240b8a8477e23
Circadence <45575199+carloscontrerashernadez@users.noreply.github.com>
2021-05-05 14:18:05 -0600

    removed check for site settings

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -74,38 +74,15 @@ export const query = graphql`
 `;
 */
 const IndexPage = (props) => {
-  const { data, errors } = props;
+ 
 
-  if (errors) {
-    return (
-      <Layout>
-        <GraphQLErrorList errors={errors} />
-      </Layout>
-    );
-  }
-
-  const site = (data || {}).site;
-  const postNodes = (data || {}).posts
-    ? mapEdgesToNodes(data.posts)
-        .filter(filterOutDocsWithoutSlugs)
-        .filter(filterOutDocsPublishedInTheFuture)
-    : [];
-
-  if (!site) {
-    throw new Error(
-      'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
-    );
-  }
+ 
 
   return (
     <ChakraProvider theme={theme}>
     <Layout>
 
-      <SEO
-        title={site.title}
-        description={site.description}
-        keywords={site.keywords}
-      />
+ 
 <IndexHero/>
 <IndexTestimonials/>
     </Layout>
